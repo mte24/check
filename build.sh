6177df382b05488e64c8a401fe711ebf4793c707
Mark Elvers <mark.elvers@tunbury.org>
2025-01-21 17:26:39 +0000

    detect solver failure

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -o pipefail
+
 for pkg in $(opam list --all --short) ; do
   if [ ! -f good/$pkg ] && [ ! -f bad/$pkg ] ; then
     echo Building $pkg
-    docker build --build-arg PKG=$pkg --progress=plain -t test docker &> $pkg
+    docker build --build-arg PKG=$pkg --progress=plain -t test docker 2>&1 | tee $pkg
     if [ $? -eq 0 ] ; then
       docker rmi test
       mv $pkg good
